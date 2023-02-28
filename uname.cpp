4496f72619d94ab849daaa9d475c087e8e9c470d
Finisher <112564342+fini03@users.noreply.github.com>
2023-02-28 21:04:14 +0100

    Update uname.cpp

--- uname.cpp
+++ uname.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <sys/utsname.h>
+
 int main() {
-	struct utsname sysinfo;
-	uname(&sysinfo);
-	std::cout << "OS: " << sysinfo.sysname << "\n";
-	std::cout << "Hostname: " << sysinfo.nodename << "\n";
-	std::cout << "Release: " << sysinfo.release << "\n";
-	std::cout << "Version: " << sysinfo.version << "\n";
-	return 0;
+    // Declare a struct to hold system information
+    struct utsname sysinfo;
+    // Call uname() to populate the sysinfo struct with the current system information
+    uname(&sysinfo);
+
+    // Output the name of the operating system
+    std::cout << "OS: " << sysinfo.sysname << "\n";
+    // Output the hostname of the current system
+    std::cout << "Hostname: " << sysinfo.nodename << "\n";
+    // Output the release version of the operating system
+    std::cout << "Release: " << sysinfo.release << "\n";
+    // Output the version of the operating system
+    std::cout << "Version: " << sysinfo.version << "\n";
+
+    // Return 0 to indicate successful execution
+    return 0;
 }
