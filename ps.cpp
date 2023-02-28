2850cde31394eea4509d485dea89675106b64c18
Finisher <112564342+fini03@users.noreply.github.com>
2023-02-28 20:38:16 +0100

    Update ps.cpp

--- ps.cpp
+++ ps.cpp
@@ -1,100 +1,89 @@
 #include <iostream>
-#include <filesystem>
-#include <math.h>
-#include <sstream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include<vector>
 #include <fstream>
+#include <filesystem>
 #include <algorithm>
-#include <dirent.h>
-extern "C" {
-  #include <unistd.h>
-	#include <sys/syscall.h>
-}
-using namespace std;
-namespace fs = filesystem;
-using namespace fs;
-bool isNumber(const string& str) {
-	for(char const &c : str) {
-		if(std::isdigit(c) == 0) return false;
-  }
-  return true;
-}
+#include <string>
+#include <vector>
+
+namespace fs = std::filesystem;
 
 int main() {
-	cout << "[";
-	path proc("/proc");
-	bool first = true;
-	for(const auto& entry : directory_iterator(proc)) {
-		if(isNumber(entry.path().filename())) {
-			string pid = entry.path().filename();
-			path exe("/proc/" + pid + "/exe"), cwd("/proc/" + pid + "/cwd"), base("/proc/" + pid + "/map_files");
-			try {
-				if(exists(exe) && exists(cwd)) {          
-					if(first) {
-						first = false;
-					}
-					else {
-						cout << ",";
-					}
-					cout << "{" << "\"pid\":" << pid << "," << "\"exe\":" << read_symlink(exe) << "," << "\"cwd\":" << read_symlink(cwd) << ",";
-					vector<unsigned long int> adress = {};
-					for(const auto& entry1 : directory_iterator(base)) {
-						string name = entry1.path().filename();
-						string address = name.substr(0, name.find("-"));
-						unsigned long int x;
-						stringstream s1;
-						s1 << hex << address;
-						s1 >> x;
-						adress.push_back(x);  
-					}
-					unsigned long int min = *min_element(adress.begin(), adress.end(),
-																							 [&] (const unsigned long int& a, const unsigned long int& b) {
-																											return a < b;
-																							 });          
-					cout << "\"base_adresse\":" << min << ",";
-					ifstream status("/proc/" + pid + "/status");
-					vector<string> state = {};
-					for(string line; getline(status, line);) {
-						state.push_back(line); 
-					}                   
-					string str = state.at(2);
-					cout << "\"state\":" << "\"" << str.at(7) << "\",";
-					ifstream cmdline("/proc/" + pid + "/cmdline");
-					vector<string> help = {};
-					for(string line; getline(cmdline, line);) {
-						help.push_back(line);
-					}
-					string line2 = help.at(0);
-					vector<string> commandline = {};
-					string current_cmd = "";
-					for(size_t i = 0; i < line2.length(); i++) {
-						if(line2[i] == '\0') {
-							commandline.push_back(current_cmd);
-							current_cmd = "";
-						}
-						else {
-							current_cmd += line2[i];
-						}
-					}
-					cout << "\"cmdline\":[";
-					for(size_t i = 0; i < commandline.size(); i++) {
-						cout << "\"" << commandline.at(i) << "\"";
-						if(i != commandline.size()-1) {
-							cout << ",";
-						}
-					}
-					cout << "]}";
-				}
-			} catch(const std::exception& e) {
-				continue;
-			}
-		}      
-	}
-	cout << "]";
-  return 0;
+    std::cout << "[";
+
+    bool first = true;
+    // Iterate through all processes in the /proc directory
+    for (const auto& entry : fs::directory_iterator("/proc")) {
+        // Get the process ID from the directory name
+        const std::string& pid = entry.path().filename().string();
+        // Ignore directories that do not contain a numeric process ID
+        if (!std::all_of(pid.begin(), pid.end(), ::isdigit)) {
+            continue;
+        }
+
+        // Construct file system paths for the process's executable, current working directory, and memory map
+        fs::path proc = "/proc";
+        const fs::path exe = proc / pid / "exe";
+        const fs::path cwd = proc / pid / "cwd";
+        const fs::path base = proc / pid / "map_files";
+
+        try {
+            // Check that the process's executable and current working directory exist
+            if (fs::exists(exe) && fs::exists(cwd)) {
+                if (first) {
+                    first = false;
+                } else {
+                    std::cout << ",";
+                }
+
+                // Print the process's PID, executable path, current working directory, and base address of its memory map
+                std::cout << "{\"pid\":" << pid << ","
+                          << "\"exe\":\"" << fs::read_symlink(exe).string() << "\","
+                          << "\"cwd\":\"" << fs::read_symlink(cwd).string() << "\",";
+                // Get the base addresses of all memory map files for the process
+                std::vector<unsigned long> addresses;
+                for (const auto& map_entry : fs::directory_iterator(base)) {
+                    const std::string& map_file = map_entry.path().filename().string();
+                    const std::string& address_str = map_file.substr(0, map_file.find('-'));
+
+                    // Convert hexadecimal string to unsigned long
+                    unsigned long address = std::stoul(address_str, nullptr, 16);
+
+                    addresses.push_back(address);
+                }
+
+                // Print the process's state (from /proc/[pid]/status)
+                std::string line;
+                std::ifstream status_file("/proc/" + pid + "/status");
+                while (std::getline(status_file, line)) {
+                    if (line.find("State:") == 0) {
+                        std::string state = line.substr(7, 1); // extract first character of state
+                        std::cout << "\"state\":\"" << state << "\",";
+                        break;
+                    }
+                }
+
+                // Print the process's command line (from /proc/[pid]/cmdline)
+                std::ifstream cmdline("/proc/" + pid + "/cmdline");
+                std::vector<std::string> command_line;
+                std::string arg;
+                while (std::getline(cmdline, arg, '\0')) {
+                    command_line.push_back(arg);
+                }
+                std::cout << "\"base_address\":" << *std::min_element(addresses.begin(), addresses.end()) << ","
+                          << "\"cmdline\":[";
+                for (size_t i = 0; i < command_line.size(); i++) {
+                    std::cout << "\"" << command_line[i] << "\"";
+                    if (i < command_line.size() - 1) {
+                        std::cout << ",";
+                    }
+                }
+                std::cout << "]}";
+            }
+        } catch(const std::exception& e) {
+            // Ignore any exceptions thrown while processing a process directory
+            continue;
+        }
+    }
+    std::cout << "]";
+    return 0;
 }
